9af3080998074682b86faf88bad20f6c3b51a655
jucjeff <zma67@wisc.edu>
2018-08-01 13:37:34 -0500

    First commit

---
+++ src/js/app.js
@@ -0,0 +1,39 @@
+App = {
+     web3Provider: null,
+     contracts: {},
+
+     init: function() {
+          //load articlesRow
+          var articlesRow = $('#articlesRow');
+          var articleTemplate = $('#articleTemplate');
+
+          articleTemplate.find('.panel-title').text('article 1');
+          articleTemplate.find('.article-description').text('Description for article 1');
+          articleTemplate.find('.article-price').text('10.23');
+          articleTemplate.find('.article-seller').text('0x12345678901234567890');
+
+          articlesRow.append(articleTemplate.html());
+
+          return App.initWeb3();
+     },
+
+     initWeb3: function() {
+          /*
+           * Replace me...
+           */
+
+          return App.initContract();
+     },
+
+     initContract: function() {
+          /*
+           * Replace me...
+           */
+     },
+};
+
+$(function() {
+     $(window).load(function() {
+          App.init();
+     });
+});
