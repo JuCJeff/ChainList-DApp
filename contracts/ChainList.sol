908c1f8386a902ab68da60ca8b992946937006a8
jucjeff <zma67@wisc.edu>
2018-08-31 15:13:13 -0500

    Deleted unused modules

--- contracts/ChainList.sol
+++ contracts/ChainList.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.4.18;
 
-contract ChainList {
+import "./Ownable.sol";
+
+contract ChainList is Ownable {
   // custom types
   struct Article {
     uint id;
@@ -12,6 +14,7 @@ contract ChainList {
   }
 
   // state variables
+  address owner;
   mapping (uint => Article) public articles;
   uint articleCounter;
 
@@ -30,6 +33,20 @@ contract ChainList {
     string _name,
     uint256 _price
     );
+
+    modifier onlyOwner() {
+      require(msg.sender == owner);
+      _;
+    }
+
+  function ChainList() public {
+    owner = msg.sender;
+  }
+
+  // deactivate the contract
+  function kill() public onlyOwner{
+    selfdestruct(owner);
+  }
 
   // sell an article
   function sellArticle(string _name, string _description, uint256 _price) public {
