15ecd3012514769987247d3214b695068766cf4a
jucjeff <zma67@wisc.edu>
2018-09-23 13:22:38 -0500

    adding frontend files to Github pages

--- startrinkeby-linux.sh
+++ startrinkeby-linux.sh
@@ -1,2 +1,1 @@
-geth --rinkeby --rpc --rpcapi="personal,eth,network,web3,net" --ipcpath
-"~/Desktop/.ethereum/geth.ipc"
+geth --rinkeby --rpc --rpcapi="personal,eth,network,web3,net" --ipcpath "~/.ethereum/geth.ipc"
